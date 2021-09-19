f9c4ec460608de29428de578ab0648238fad6553
yudii2 <qleen513@gmail.com>
2021-09-19 16:25:09 +0900

    [김유진] 공통모듈 수정

--- src/com/togetherHiking/common/code/Config.java
+++ src/com/togetherHiking/common/code/Config.java
@@ -4,9 +4,9 @@ public enum Config {
 	
 	//DOMAIN("http://www.pclass.com"),
 	DOMAIN("http://localhost:7070"),
-	SMTP_AUTHENTICATION_ID("togetherHiking@gmail.com"),
-	SMTP_AUTHENTICATION_PASSWORD("1234"),
-	COMPANY_EMAIL("togetherHiking@gmail.com"),
+	SMTP_AUTHENTICATION_ID("khpclass1@gmail.com"),
+	SMTP_AUTHENTICATION_PASSWORD("@project1"),
+	COMPANY_EMAIL("khpclass1@gmail.com"),
 	//UPLOAD_PATH("C:\\CODE\\after\\upload\\"); 운영서버
 	UPLOAD_PATH("C:\\SEMI\\upload\\"); //개발서버
 	
