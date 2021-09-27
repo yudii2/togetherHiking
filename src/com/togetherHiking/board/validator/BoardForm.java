80951b215e02f038f422cbcec43e5593d97782c6
LeeSangYoeb98 <enql2007@naver.com>
2021-09-27 16:31:52 +0900

    [이상엽] 9/27 16:31 중간 푸시

--- src/com/togetherHiking/board/validator/BoardForm.java
+++ src/com/togetherHiking/board/validator/BoardForm.java
@@ -25,22 +25,26 @@ public class BoardForm {
 	public boolean test() {
 		boolean isFailed = false;
 		// 제목이 빈칸이라면
-		if(title.equals("") || title == null) {
+		if(title == null || title.isEmpty()) {
 			faildValidation.put("title", title);
 			isFailed = true;
+			System.out.println("title null");
 		}
 		// 말머리가 "잡담" 또는 "후기" 가 아니라면
-		if(!(subject.equals("잡담") || subject.equals("후기"))) {
+		if(subject == null || !(subject.equals("잡담") || subject.equals("후기"))) {
 			faildValidation.put("subject", subject);
 			isFailed = true;
+			System.out.println("subject null");
 		}
 		// 내용이 비어있다면
-		if(content.equals("") || content == null) {
+		if(content == null || content.isEmpty()) {
 			faildValidation.put("content", content);
 			isFailed = true;
+			System.out.println("content null");
 		}
 		
 		if(isFailed) {
+			System.out.println("BoardForm.java 에러 발생");
 			request.getSession().setAttribute("boardValid",faildValidation);
 			request.getSession().setAttribute("boardForm",this);
 			return false;
